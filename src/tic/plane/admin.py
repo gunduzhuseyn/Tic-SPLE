1804ca8ea7b984c628368402ffd961077619ea62
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2018-04-23 11:29:10 +0300

    Plane version

--- src/tic/plane/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import PlaneEvent, SeatingPlan, Seat
-
-admin.site.register(PlaneEvent)
-admin.site.register(SeatingPlan)
-admin.site.register(Seat)
--- src/tic/plane/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import (CreatePlaneEventView, CreateSeatingPlanView)
-urlpatterns = [
-    path('', include('core.urls')),
-    path('event/new/', CreatePlaneEventView.as_view(), name='create_plane_event'),
-    path('event/new/seating_plan/', CreateSeatingPlanView.as_view(), name='create_seating_plan')
-] 
